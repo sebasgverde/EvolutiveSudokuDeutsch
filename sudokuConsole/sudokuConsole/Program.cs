669e5c3d88541e78dc030c5fcb9f261ffbe555bf
sebastian garcia valencia <sgarci18@eafit.edu.co>
2014-12-12 23:17:48 +0100

    Erste commit, Grund kode, ich habe eine Grund Mutation, und eine erste Fitness-Aufgabe

---
+++ sudokuConsole/sudokuConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sudokuConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sudoku sud = new Sudoku();
+            sud.test();
+        }
+    }
+}
