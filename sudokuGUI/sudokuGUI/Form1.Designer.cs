d07ebe739fabbebce7ff4bf4c04f70449e22aeff
sebastian garcia valencia <sgarci18@eafit.edu.co>
2015-01-26 20:01:21 +0100

    Schoner GUI

--- sudokuGUI/sudokuGUI/Form1.Designer.cs
+++ sudokuGUI/sudokuGUI/Form1.Designer.cs
@@ -49,8 +49,6 @@
             this.eliteTxt = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.popSizTxt = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.mutMethodCB = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.mutRadTxt = new System.Windows.Forms.TextBox();
@@ -79,6 +77,9 @@
             this.restartGeneTxt = new System.Windows.Forms.TextBox();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -87,12 +88,15 @@
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
             this.SuspendLayout();
             // 
             // sudtxt
             // 
+            this.sudtxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.sudtxt.Font = new System.Drawing.Font("Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.sudtxt.Location = new System.Drawing.Point(306, 114);
+            this.sudtxt.Location = new System.Drawing.Point(6, 15);
             this.sudtxt.Multiline = true;
             this.sudtxt.Name = "sudtxt";
             this.sudtxt.Size = new System.Drawing.Size(136, 208);
@@ -102,26 +106,29 @@
             // 
             // startBtn
             // 
-            this.startBtn.Location = new System.Drawing.Point(521, 28);
+            this.startBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.startBtn.Location = new System.Drawing.Point(540, 31);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(73, 22);
             this.startBtn.TabIndex = 1;
             this.startBtn.Text = "Beginnen";
-            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.UseVisualStyleBackColor = false;
             this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
             // 
             // resultTxt
             // 
+            this.resultTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.resultTxt.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultTxt.Location = new System.Drawing.Point(528, 80);
+            this.resultTxt.Location = new System.Drawing.Point(11, 15);
             this.resultTxt.Multiline = true;
             this.resultTxt.Name = "resultTxt";
             this.resultTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.resultTxt.Size = new System.Drawing.Size(238, 290);
+            this.resultTxt.Size = new System.Drawing.Size(243, 290);
             this.resultTxt.TabIndex = 2;
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label6);
@@ -136,7 +143,7 @@
             this.groupBox1.Controls.Add(this.eliteTxt);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.popSizTxt);
-            this.groupBox1.Location = new System.Drawing.Point(35, 93);
+            this.groupBox1.Location = new System.Drawing.Point(35, 70);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(207, 229);
             this.groupBox1.TabIndex = 3;
@@ -172,6 +179,7 @@
             // 
             // crroChanTxt
             // 
+            this.crroChanTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.crroChanTxt.Location = new System.Drawing.Point(132, 162);
             this.crroChanTxt.Name = "crroChanTxt";
             this.crroChanTxt.Size = new System.Drawing.Size(31, 20);
@@ -190,6 +198,7 @@
             // 
             // mutChanTxt
             // 
+            this.mutChanTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.mutChanTxt.Location = new System.Drawing.Point(132, 136);
             this.mutChanTxt.Name = "mutChanTxt";
             this.mutChanTxt.Size = new System.Drawing.Size(31, 20);
@@ -208,6 +217,7 @@
             // 
             // maxPopTxt
             // 
+            this.maxPopTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.maxPopTxt.Location = new System.Drawing.Point(132, 110);
             this.maxPopTxt.Name = "maxPopTxt";
             this.maxPopTxt.Size = new System.Drawing.Size(69, 20);
@@ -226,6 +236,7 @@
             // 
             // maxGenTxt
             // 
+            this.maxGenTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.maxGenTxt.Location = new System.Drawing.Point(132, 84);
             this.maxGenTxt.Name = "maxGenTxt";
             this.maxGenTxt.Size = new System.Drawing.Size(69, 20);
@@ -244,6 +255,7 @@
             // 
             // eliteTxt
             // 
+            this.eliteTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.eliteTxt.Location = new System.Drawing.Point(132, 58);
             this.eliteTxt.Name = "eliteTxt";
             this.eliteTxt.Size = new System.Drawing.Size(69, 20);
@@ -262,6 +274,7 @@
             // 
             // popSizTxt
             // 
+            this.popSizTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.popSizTxt.Location = new System.Drawing.Point(132, 32);
             this.popSizTxt.Name = "popSizTxt";
             this.popSizTxt.Size = new System.Drawing.Size(69, 20);
@@ -269,26 +282,9 @@
             this.popSizTxt.Text = "1500";
             this.popSizTxt.TextChanged += new System.EventHandler(this.popSizTxt_TextChanged);
             // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(303, 93);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(46, 13);
-            this.label9.TabIndex = 4;
-            this.label9.Text = "Eingabe";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(534, 57);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(53, 13);
-            this.label10.TabIndex = 5;
-            this.label10.Text = "Ergebniss";
-            // 
             // mutMethodCB
             // 
+            this.mutMethodCB.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.mutMethodCB.FormattingEnabled = true;
             this.mutMethodCB.Items.AddRange(new object[] {
             "Random Swap",
@@ -302,6 +298,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.groupBox2.Controls.Add(this.mutRadTxt);
             this.groupBox2.Controls.Add(this.Radius);
             this.groupBox2.Controls.Add(this.Methode);
@@ -315,6 +312,7 @@
             // 
             // mutRadTxt
             // 
+            this.mutRadTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.mutRadTxt.Location = new System.Drawing.Point(110, 49);
             this.mutRadTxt.Name = "mutRadTxt";
             this.mutRadTxt.Size = new System.Drawing.Size(23, 20);
@@ -342,6 +340,7 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.groupBox3.Controls.Add(this.label11);
             this.groupBox3.Controls.Add(this.crossMetCB);
             this.groupBox3.Location = new System.Drawing.Point(26, 474);
@@ -362,6 +361,7 @@
             // 
             // crossMetCB
             // 
+            this.crossMetCB.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.crossMetCB.FormattingEnabled = true;
             this.crossMetCB.Items.AddRange(new object[] {
             "1-Punkt",
@@ -374,6 +374,7 @@
             // 
             // groupBox4
             // 
+            this.groupBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.groupBox4.Controls.Add(this.turnIndTxt);
             this.groupBox4.Controls.Add(this.label13);
             this.groupBox4.Controls.Add(this.label12);
@@ -387,6 +388,7 @@
             // 
             // turnIndTxt
             // 
+            this.turnIndTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.turnIndTxt.Location = new System.Drawing.Point(132, 52);
             this.turnIndTxt.Name = "turnIndTxt";
             this.turnIndTxt.Size = new System.Drawing.Size(31, 20);
@@ -414,6 +416,7 @@
             // 
             // selMethoCB
             // 
+            this.selMethoCB.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.selMethoCB.FormattingEnabled = true;
             this.selMethoCB.Items.AddRange(new object[] {
             "Turnierauswahl",
@@ -426,6 +429,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.textBox1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(6, 3);
             this.textBox1.Multiline = true;
@@ -436,27 +440,30 @@
             // 
             // weiterSchrittBtn
             // 
-            this.weiterSchrittBtn.Location = new System.Drawing.Point(600, 28);
+            this.weiterSchrittBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.weiterSchrittBtn.Location = new System.Drawing.Point(619, 31);
             this.weiterSchrittBtn.Name = "weiterSchrittBtn";
             this.weiterSchrittBtn.Size = new System.Drawing.Size(75, 23);
             this.weiterSchrittBtn.TabIndex = 22;
             this.weiterSchrittBtn.Text = "->";
-            this.weiterSchrittBtn.UseVisualStyleBackColor = true;
+            this.weiterSchrittBtn.UseVisualStyleBackColor = false;
             this.weiterSchrittBtn.Click += new System.EventHandler(this.button2_Click);
             // 
             // weiterGehenTxt
             // 
-            this.weiterGehenTxt.Location = new System.Drawing.Point(681, 28);
+            this.weiterGehenTxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.weiterGehenTxt.Location = new System.Drawing.Point(700, 31);
             this.weiterGehenTxt.Name = "weiterGehenTxt";
             this.weiterGehenTxt.Size = new System.Drawing.Size(75, 23);
             this.weiterGehenTxt.TabIndex = 23;
             this.weiterGehenTxt.Text = ">>>";
-            this.weiterGehenTxt.UseVisualStyleBackColor = true;
+            this.weiterGehenTxt.UseVisualStyleBackColor = false;
             this.weiterGehenTxt.Click += new System.EventHandler(this.weiterGehenTxt_Click);
             // 
             // shritteTxt
             // 
-            this.shritteTxt.Location = new System.Drawing.Point(651, 2);
+            this.shritteTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.shritteTxt.Location = new System.Drawing.Point(666, 5);
             this.shritteTxt.Name = "shritteTxt";
             this.shritteTxt.Size = new System.Drawing.Size(45, 20);
             this.shritteTxt.TabIndex = 24;
@@ -464,6 +471,7 @@
             // 
             // popFitTxt
             // 
+            this.popFitTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.popFitTxt.Location = new System.Drawing.Point(3, 6);
             this.popFitTxt.Multiline = true;
             this.popFitTxt.Name = "popFitTxt";
@@ -473,6 +481,7 @@
             // 
             // genFitTxt
             // 
+            this.genFitTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.genFitTxt.Location = new System.Drawing.Point(119, 6);
             this.genFitTxt.Multiline = true;
             this.genFitTxt.Name = "genFitTxt";
@@ -484,7 +493,7 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(528, 376);
+            this.tabControl1.Location = new System.Drawing.Point(11, 314);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(242, 233);
@@ -515,6 +524,7 @@
             // 
             // groupBox5
             // 
+            this.groupBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.groupBox5.Controls.Add(this.methodeRestCB);
             this.groupBox5.Controls.Add(this.label16);
             this.groupBox5.Controls.Add(this.restartGeneTxt);
@@ -528,6 +538,7 @@
             // 
             // methodeRestCB
             // 
+            this.methodeRestCB.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.methodeRestCB.FormattingEnabled = true;
             this.methodeRestCB.Items.AddRange(new object[] {
             "Restart",
@@ -550,6 +561,7 @@
             // 
             // restartGeneTxt
             // 
+            this.restartGeneTxt.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.restartGeneTxt.Location = new System.Drawing.Point(92, 29);
             this.restartGeneTxt.Name = "restartGeneTxt";
             this.restartGeneTxt.Size = new System.Drawing.Size(31, 20);
@@ -569,32 +581,63 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(597, 5);
+            this.label15.Location = new System.Drawing.Point(612, 8);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(43, 13);
             this.label15.TabIndex = 28;
             this.label15.Text = "Schritte";
             // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(-1, 609);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(162, 13);
+            this.label17.TabIndex = 29;
+            this.label17.Text = "Sebastian Garcia Valencia, 2015";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox6.Controls.Add(this.sudtxt);
+            this.groupBox6.Location = new System.Drawing.Point(321, 70);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(152, 229);
+            this.groupBox6.TabIndex = 30;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Eingabe";
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox7.Controls.Add(this.resultTxt);
+            this.groupBox7.Controls.Add(this.tabControl1);
+            this.groupBox7.Location = new System.Drawing.Point(521, 70);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(263, 552);
+            this.groupBox7.TabIndex = 31;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Ergebniss";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 621);
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(796, 626);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.label17);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.groupBox5);
-            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.shritteTxt);
             this.Controls.Add(this.weiterGehenTxt);
             this.Controls.Add(this.weiterSchrittBtn);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.resultTxt);
             this.Controls.Add(this.startBtn);
-            this.Controls.Add(this.sudtxt);
             this.Name = "Form1";
             this.Text = "Sudoku, eine Evolutionäre Lösung";
             this.groupBox1.ResumeLayout(false);
@@ -612,6 +655,10 @@
             this.tabPage2.PerformLayout();
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -637,8 +684,6 @@
         private System.Windows.Forms.TextBox crroChanTxt;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.ComboBox mutMethodCB;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
@@ -667,6 +712,9 @@
         private System.Windows.Forms.TextBox restartGeneTxt;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.GroupBox groupBox7;
     }
 }
 
